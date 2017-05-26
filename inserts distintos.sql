34155c44378d595a1357f2c22143b4166d5cbadd
Roberto <jrggroberto@gmail.com>
2017-05-26 10:18:14 -0300

    Create inserts distintos.sql

---
+++ inserts distintos.sql
@@ -0,0 +1,12 @@
+insert into TMPJRGG(Idteste, Nome, Descri) values (1,'AAAAA', 'varios As');
+
+
+
+insert all
+into TMPJRGG(Idteste, Nome, Descri)
+  values(2,'BBBBB', 'varios Bs')
+into TMPJRGG(Idteste, Nome, Descri)
+  values(3,'CCCCC', 'varios Cs')
+into TMPJRGG(Idteste, Nome, Descri) 
+  values(4,'DDDDD', 'varios Ds')
+select * from dual;
